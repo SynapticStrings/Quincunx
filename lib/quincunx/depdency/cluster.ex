a032a1dbec164f0d240691f3bdbbe9a64be34da8
RoastChestnut <chestnut.roasted@outlook.com>
2026-02-24 14:35:00 +0800

    feat: 移除混色策略相关代码，简化节点颜色处理逻辑

--- lib/quincunx/depdency/cluster.ex
+++ lib/quincunx/depdency/cluster.ex
@@ -7,26 +7,11 @@ defmodule Quincunx.Dependency.Cluster do
   alias Quincunx.Dependency.Node
   @type cluster_name :: atom() | String.t()
 
-  @type color_mixing_strategy ::
-          :isolate
-          # 强行并入某个特定的簇
-          | {:merge_into, cluster_name()}
-          # 继承输入中最重/节点最多的簇颜色
-          | :dominant_upstream
-
   @type t :: %__MODULE__{
-          # 用户/前端显式指定的节点簇映射 (如 %{:acoustic_model => :cluster_1})
           node_colors: %{Node.name() => cluster_name()},
-
-          # 混色解决策略：当节点依赖了 [cluster_A, cluster_B] 时怎么处理？
-          mixing_strategy: color_mixing_strategy(),
-
-          # 用户决定要在这一层直接合并的簇列表，比如 [[:cluster_1, :cluster_2], [:cluster_3]]
-          # 这样 Compiler 就会吐出 2 个 Recipe 而不是 3 个
-          merge_groups: [{[cluster_name()], cluster_name()}]
+          merge_groups: [{[cluster_name()] | MapSet.t(cluster_name()), cluster_name()}]
         }
   defstruct node_colors: %{},
-            mixing_strategy: :isolate,
             merge_groups: []
 
   @doc "Return `%{node_name => final_cluster_name}`"
@@ -40,10 +25,7 @@ defmodule Quincunx.Dependency.Cluster do
           Map.put(color_map, node.name, explicit_color)
 
         true ->
-          upstream_colors = get_upstream_colors(node, graph, color_map)
-
-          final_color = resolve_color(node, upstream_colors, clusters.mixing_strategy)
-          Map.put(color_map, node.name, final_color)
+          Map.put(color_map, node.name, get_upstream_colors(node, graph, color_map))
       end
     end)
   end
@@ -55,13 +37,8 @@ defmodule Quincunx.Dependency.Cluster do
       _ = upper_edges -> Enum.map(upper_edges, fn e -> e.from_node end)
     end
     |> Enum.map(&Map.get(color_map, &1, :default_cluster))
+    # [[:foo, :bar], :bar] => [:foo, :bar]
+    |> List.flatten()
+    |> Enum.uniq()
   end
-
-  defp resolve_color(_node, [], _), do: :default_cluster
-  defp resolve_color(_node, [single_color], _), do: single_color
-
-  defp resolve_color(node, _multiple_colors, :isolate),
-    do: String.to_atom("#{node.name}_isolated_cluster")
-
-  defp resolve_color(_node, _multiple_colors, {:merge_into, target_cluster}), do: target_cluster
 end
