765e18b1d8c867f366b4495d04034a3e9ee8c325
RoastChestnut <30802664+GES233@users.noreply.github.com>
2026-03-24 09:42:35 +0800

    Refactor via_tuple to use SessionRegistry directly

--- lib/quincunx/session/instance_supervisor.ex
+++ lib/quincunx/session/instance_supervisor.ex
@@ -1,12 +1,9 @@
 defmodule Quincunx.Session.InstanceSupervisor do
   use Supervisor
-
-  def via_tuple(session_id, role) do
-    {:via, Registry, {Quincunx.SessionRegistry, {session_id, role}}}
-  end
+  alias Quincunx.SessionRegistry
 
   def start_link(session_id, opts) do
-    name = via_tuple(session_id, :instance_sup)
+    name = SessionRegistry.via(session_id, :instance_sup)
 
     Supervisor.start_link(__MODULE__, {session_id, opts}, name: name)
   end
@@ -15,7 +12,7 @@ defmodule Quincunx.Session.InstanceSupervisor do
   def init({session_id, opts}) do
     children = [
       {OrchidSymbiont.Runtime, session_id: session_id},
-      {Task.Supervisor, name: via_tuple(session_id, :task_sup)},
+      {Task.Supervisor, name: SessionRegistry.via(session_id, :task_sup)},
       {Quincunx.Session.Server, Keyword.put(opts, :session_id, session_id)}
     ]
 
