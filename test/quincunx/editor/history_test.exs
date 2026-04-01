be6b9863f96cbc8c3dd2a936b9884a598f27e0dd
RoastedChestnut <chestnut.roasted@outlook.com>
2026-04-01 15:25:42 +0800

    test: add unit tests for editor history
    Implement unit tests for the history management system in the editor to ensure undo and redo functionality works as expected.
    
    - Add tests for `push/2` to verify operations are correctly added to the undo stack
    - Implement tests for `undo/1` and `redo/1` to verify state transitions
    - Add a test case for handling undo/redo on an empty history
    - Fix typos in existing operation validation tests (refult -> refute)

--- test/quincunx/editor/history_test.exs
+++ test/quincunx/editor/history_test.exs
@@ -2,6 +2,7 @@ defmodule Quincunx.Editor.HistoryTest do
   use ExUnit.Case
 
   alias Quincunx.Topology.Graph.{Node, Edge}
+  import Quincunx.Editor.History
 
   describe "Operation" do
     import Quincunx.Editor.History.Operation
@@ -17,18 +18,41 @@ defmodule Quincunx.Editor.HistoryTest do
 
       ## No..
       refute topology?({:set_intervention, {:port, :foo, :bar}, :input, "Foo"})
-      refult topology?({:remove_intervention, {:port, "foo", "bar"}, :override})
-      refult topology?({:clear_intervention, {:port, "foo", "bar"}})
+      refute topology?({:remove_intervention, {:port, "foo", "bar"}, :override})
+      refute topology?({:clear_intervention, {:port, "foo", "bar"}})
     end
   end
 
   describe "Push operation" do
-    # ...
+    test "push/2" do
+      new = new()
+      record1 = push(new, {:add_node, %Node{}})
+      record2 = push(record1, {:set_intervention, {:port, :foo, :bar}, :input, "Foo"})
+
+      assert record1.undo_stack == [{:add_node, %Node{}}]
+
+      assert record2.undo_stack == [
+               {:set_intervention, {:port, :foo, :bar}, :input, "Foo"},
+               {:add_node, %Node{}}
+             ]
+    end
   end
 
   describe "Do, Undo and Redo" do
     test "op will drop out(copy)" do
-      # ...
+      record = new()
+       |> push({:add_node, %Node{id: :node_1}})
+       |> push({:add_node, %Node{id: :node_2}})
+       |> push({:add_edge, Edge.new(:node_1, :out, :node_2, :in)})
+       |> push({:set_intervention, {:foo, :node_1, :in}, :input, "Aha!"})
+
+       assert {undid, {:set_intervention, {:foo, :node_1, :in}, _, _}} = undo(record)
+       assert {^record, _} = redo(undid)
+    end
+
+    test "blank history will return nil" do
+      assert {_, nil} = new() |> undo()
+      assert {_, nil} = new() |> redo()
     end
   end
 end
