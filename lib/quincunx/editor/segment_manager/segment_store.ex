af7a910382b824bf0721ac86c8cedc0e4cd702e2
RoastedChestnut <chestnut.roasted@outlook.com>
2026-04-03 22:57:34 +0800

    refactor(editor): add type specifications for segment management functions

--- lib/quincunx/editor/segment_manager/segment_store.ex
+++ lib/quincunx/editor/segment_manager/segment_store.ex
@@ -3,6 +3,8 @@ defmodule Quincunx.Editor.SegmentStore do
 
   @type t :: %{Segment.id() => Segment.t()}
 
+  @spec add_segment(t(), Segment.t()) ::
+          {:error, :already_exists} | {:ok, t()}
   def add_segment(store, %Segment{id: segment_id} = new_segment) do
     if Map.has_key?(store.segments, segment_id) do
       {:error, :already_exists}
@@ -11,6 +13,7 @@ defmodule Quincunx.Editor.SegmentStore do
     end
   end
 
+  @spec remove_segment(t(), Segment.id()) :: {:error, :not_exists} | {:ok, t()}
   def remove_segment(store, segment_id) do
     if Map.has_key?(store, segment_id) do
       {:ok, Map.delete(store, segment_id)}
@@ -19,10 +22,15 @@ defmodule Quincunx.Editor.SegmentStore do
     end
   end
 
+  @spec update_segment(t(), Segment.id(), (Segment.t() -> Segment.t())) ::
+          {:error, :not_exists | :segment_id_conflict}
+          | {:ok, t()}
+          | {:segment_id_changed, t()}
   def update_segment(store, segment_id, update_fn) do
     with true <- Map.has_key?(store, segment_id) do
       new_seg = update_fn.(store[segment_id])
 
+      # If some function modified the ID.
       cond do
         new_seg.id == segment_id ->
           {:ok, %{store | segment_id => new_seg}}
